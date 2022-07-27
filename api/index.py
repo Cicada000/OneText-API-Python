e056c9e9b715affd4495830c088c996869947df1
Cicada000 <78632923+Cicada000@users.noreply.github.com>
2022-07-27 19:17:32 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,12 +4,14 @@ import json , requests , random
 class handler(BaseHTTPRequestHandler):
     
     def do_GET(self):
+        
         url = requests.get("https://onetext.cicada000.work/Data.json")
         text = url.text
         OneTextRaw = json.loads(text)
-        OneText = OneTextRaw[random.randint(0,(len(OneTextRaw)-1))]
+        
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        self.wfile.write(OneText.encode())
+        self.wfile.write(OneTextRaw.encode())
+        
         return
