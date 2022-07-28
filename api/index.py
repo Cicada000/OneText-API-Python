1943fd0cb89a871cc139aac5cc2f878954cda520
Cicada000 <78632923+Cicada000@users.noreply.github.com>
2022-07-28 23:57:49 +0800

    Use Flask Framework

--- api/index.py
+++ api/index.py
@@ -1,20 +1,29 @@
-from http.server import BaseHTTPRequestHandler
-import json , requests , random
+from flask import Flask, request
+import requests , json , random
 
-class handler(BaseHTTPRequestHandler):
+def select(a):
+    list = ["Anime","Poem","Other"]
+    if str.title(str(a)) in list :
+        return str.title(str(a))
+    else :
+        return random.choice(list)
+
+app = Flask(__name__)
+
+@app.route('/api',methods=["GET"])
+
+def return_OneText():
+
+    category = request.args.get("category")
+    category = str(category)
+    category = select(category)
+
+    url = requests.get("https://onetext.cicada000.work/" + category + ".json")
+    text = url.text
+    OneTextRaw = json.loads(text)
+    OneText = OneTextRaw[random.randint(0,(len(OneTextRaw)-1))]
+    OneText = str(OneText)
+    OneText = OneText.replace("\'","\"")
+
+    return OneText, 200, {"Content-Type":"application/json"}
     
-    def do_GET(self):
-
-        url = requests.get("https://onetext.cicada000.work/Data.json")
-        text = url.text
-        OneTextRaw = json.loads(text)
-        OneText = OneTextRaw[random.randint(0,(len(OneTextRaw)-1))]
-        OneText = str(OneText)
-        OneText = OneText.replace("\'","\"")
-        
-        self.send_response(200)
-        self.send_header('Content-type','application/json')
-        self.end_headers()
-        self.wfile.write(OneText.encode())
-        
-        return
