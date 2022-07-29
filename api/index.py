2ae3d5e1c9d0081487bf19c6c27d952f021957bd
Cicada000 <78632923+Cicada000@users.noreply.github.com>
2022-07-29 12:35:24 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -22,8 +22,8 @@ def return_OneText():
     url = requests.get("https://onetext.cicada000.work/" + category + ".json")
     text = url.text
     OneTextRaw = json.loads(text)
-    OneText = OneTextRaw[random.randint(0,(len(OneTextRaw)-1))]
-    OneText = json.dumps(OneText , sort_keys=False , indent=4 , separators=(',',':') , ensure_ascii=False)
-
-    return OneText.encode(), 200, {"Content-Type":"application/json"}
+    OneText = OneTextRaw[random.randint(0,(len(OneTextRaw) - 1))]
+    OneText = json.dumps(OneText , sort_keys = False , indent = 4 , separators = (',',':') , ensure_ascii = False)
+ 
+    return OneText.encode(), 200, {"Content-Type":"application/json" , "Charset":"UTF-8"}
     
